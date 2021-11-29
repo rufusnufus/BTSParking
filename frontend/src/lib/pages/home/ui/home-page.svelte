1ed09c4a1ccea5e042039672b1bd26d26c95562b
Lev Chelyadinov <leva181777@gmail.com>
2021-11-29 16:46:54 +0300

    feat(frontend): credit the author of a photo on the home page

--- frontend/src/lib/pages/home/ui/home-page.svelte
+++ frontend/src/lib/pages/home/ui/home-page.svelte
@@ -1,4 +1,4 @@
-<main class="flex flex-col items-center py-16 self-stretch flex-1 text-white">
+<main class="flex flex-col items-center py-16 self-stretch flex-1 text-white relative">
   <p class="text-2xl lg:text-4xl font-medium">Welcome to BTS.Parking</p>
   <p class="lg:text-xl mt-1">Park your car comfortably with us</p>
   <div class="mt-12">
@@ -6,6 +6,10 @@
       href="/login"
       class="py-2 px-12 lg:px-24 bg-red-800 rounded hover:bg-red-700">Log in</a
     >
+  </div>
+
+  <div class="px-3 py-1 absolute left-4 bottom-4 rounded-md bg-black bg-opacity-40 font-light text-sm">
+    Photo by <a href="https://unsplash.com/@jordankgraff?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jordan Graff</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   </div>
 </main>
 
@@ -15,5 +19,9 @@
     background-size: cover;
     background-position-x: center;
     background-position-y: center;
+  }
+
+  a {
+    text-decoration: underline;
   }
 </style>
