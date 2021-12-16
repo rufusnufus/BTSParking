5ea4f4ca8a63afe9ce808303acd2100953f5f06c
Lev Chelyadinov <leva181777@gmail.com>
2021-12-16 17:12:02 +0300

    feat(frontend): switch to the real endpoint for zones

--- frontend/src/lib/pages/zone/index.ts
+++ frontend/src/lib/pages/zone/index.ts
@@ -1,3 +1,2 @@
-// export { fetchZoneMap } from './model/fetch-zone-map';
-export { mockFetchZoneMap as fetchZoneMap } from './model/mock-fetch-zone-map';
+export { fetchZoneMap } from './model/fetch-zone-map';
 export { default as ZonePage } from './ui/zone-page.svelte';
