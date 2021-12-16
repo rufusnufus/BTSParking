dc4012a27f32400dade0458f55d1329e28edc14b
Lev Chelyadinov <leva181777@gmail.com>
2021-12-16 19:24:40 +0300

    fix(frontend): prevent development errors from `prom-client`

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -3,7 +3,11 @@ import { collectDefaultMetrics } from 'prom-client';
 import { identifySelf } from '$lib/processes/auth';
 import { logRequestsHook, logRenderErrorsHook } from '$lib/processes/logging';
 
-collectDefaultMetrics();
+try {
+  collectDefaultMetrics();
+} catch (e) {
+  // metrics are already registered, do nothing
+}
 
 export const getSession = identifySelf;
 
