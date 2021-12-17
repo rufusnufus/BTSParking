ad285d4f06508153a2159c1f58a4a5b3c2bb34f2
Lev Chelyadinov <leva181777@gmail.com>
2021-12-17 16:25:46 +0300

    feat(frontend): add an empty state for the statistics page

--- frontend/src/lib/pages/statistics/ui/statistics-page.svelte
+++ frontend/src/lib/pages/statistics/ui/statistics-page.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+  import IconStats from '~icons/bx/bx-stats';
+
   import { StatisticsGrid } from '$lib/widgets/statistics-grid';
+  import { EmptyState } from '$lib/shared/ui';
   import type { Statistics } from '$lib/shared/api';
 
   export let statistics: Statistics[];
 </script>
 
-<StatisticsGrid {statistics} />
+<div class="flex-1 flex flex-col items-center justify-center">
+  {#if statistics.length > 0}
+    <StatisticsGrid {statistics} />
+  {:else}
+    <EmptyState icon={IconStats}>
+      <p>Nothing to display.</p>
+      <p>Book a parking space and see it here!</p>
+    </EmptyState>
+  {/if}
+</div>
